e1b9cb7e09e99a6d3c6ec091576e3e6450031d4b
samircd4 <samircd4@gmail.com>
2022-10-10 15:58:12 +0600

    first data scrapped

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# Hello Emmanual
+
 # Let's Start
 #1 Make a request to get data from ebay.com
 #2 Collect data from each detail page
@@ -5,13 +7,47 @@
 #4 Write scrapped data to a csv file
 
 
+from urllib import response
+import requests
+from bs4 import BeautifulSoup as bs
 
+def get_page(url):
+    response = requests.get(url)
+    if not response.ok:
+        print('Server responded:', response.status_code)
+    else:
+        soup = bs(response.text, 'html.parser')
+    return soup
+
+def get_detail_data(soup):
+    title = ''
+    price = 0
+    sold_item = 0
+    try:
+        title = soup.find('h1', class_='x-item-title__mainTitle').find('span').text
+    except:
+        title = ''
+        
+    try:
+        price = soup.find('div', id='vi-mskumap-none').find('span').text.split(' ')[1]
+    except:
+        price = ''
+    try:
+        sold = soup.find('span', class_='vi-qtyS-hot-red').find('a').text.split(' ')[0].replace(',','')
+    except:
+        sold = ''
+    data = {
+        'title': title,
+        'price': price,
+        'sold': sold
+    }
+    return data
 
 
 def main():
-    pass
-
-
+    url = 'https://www.ebay.com/itm/263038140739?hash=item3d3e4b8143:g:IIEAAOSwN1peYA5n&amdata=enc%3AAQAHAAAAoH03XKH12LLbrs0QUg6AxhV8F%2BHnWEpUcPz1TUxwEooIGiJ%2BQyLV%2Bpp3dzVgNaj5QFG33zwASqwvj0PfsIx8G9yvB2PqhuCRJtoIUp3fuZVUDdoPdkAWDYKA2LNhrnEMakz1w5VlRyRDePwBWPwK8N%2BZeQwbkz7HAejtGJO4kZdNSHpi8lH4b8xCbfH3h4VabgeZSPvFT1%2Bxxk%2F%2B1YQNpUA%3D%7Ctkp%3ABk9SR6j9npL4YA'
+    get_page(url)
+    print(get_detail_data(get_page(url)))
 
 
 
