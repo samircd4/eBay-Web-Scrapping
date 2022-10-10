85d25463dc7a04c41ea68ceb588aa467b8668d86
samircd4 <samircd4@gmail.com>
2022-10-10 14:46:41 +0600

    Instruction added

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+# Let's Start
+#1 Make a request to get data from ebay.com
+#2 Collect data from each detail page
+#3 Collect all link to detail page of each product
+#4 Write scrapped data to a csv file
+
+
+
+
+
 def main():
     pass
 
